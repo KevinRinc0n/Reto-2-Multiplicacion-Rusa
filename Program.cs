1fac4cb8a43b25c836bf2686f34f941a16a1bc3e
KevinRinc0n <rinconkdavid@gmail.com>
2023-12-04 18:50:49 -0500

    feat: :sparkles: finished

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿static int multiplicacionRusa(int a, int b)
+{
+    int resultado = 0;
+    while(a > 0)
+    {
+        if(a % 2 !=0)
+        {
+            resultado += b;
+        }
+        a /= 2;
+        b *= 2;
+    }
+    return resultado;
+}
+Console.Write("Ingresa el valor del multiplicador: ");
+int v1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ingresa el valor del multiplicando: ");
+int v2 = Convert.ToInt32(Console.ReadLine());
+
+int resultado = multiplicacionRusa(v1,v2);
+Console.WriteLine(resultado);
